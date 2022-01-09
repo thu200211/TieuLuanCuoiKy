8fb79f12f371a858af52af76e4aef7dada737fba
thu200211 <97327188+thu200211@users.noreply.github.com>
2022-01-09 15:20:12 +0700

    Add files via upload

---
+++ sanpham.java
@@ -0,0 +1,48 @@
+package com.thuntv2008110121.kiemtracuoiki1;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Sanhsu {
+	int maHang;
+	String tenSanPham;
+	float giaTri;
+	Date ngayNhapKho;
+
+	Sanhsu next;
+
+	Sanhsu() {
+
+	}
+
+	Sanhsu(int _id, String _ten, float _gia, Date _ngayNhapKho) {
+		maHang = _id;
+		tenSanPham = _ten;
+		giaTri = _gia;
+		ngayNhapKho = _ngayNhapKho;
+	}
+
+	public void NhapTT(Scanner scanner) {
+
+		scanner.nextLine();
+		System.out.print("Nhap ten san pham: ");
+		tenSanPham = scanner.nextLine();
+
+		System.out.print("Hay nhap gia san pham: ");
+		giaTri = scanner.nextFloat();
+
+		ngayNhapKho = new Date();
+	}
+
+	public void inTT() {
+		System.out.println(
+				"Ten : " + tenSanPham + ", Loai : Sanh su, Id: " + maHang + ", Gia: " + giaTri + ", Ngay nhap: "
+						+ ngayNhapKho);
+	}
+
+	public String GetName() {
+		System.out.println(tenSanPham);
+		return tenSanPham;
+	}
+
+}
